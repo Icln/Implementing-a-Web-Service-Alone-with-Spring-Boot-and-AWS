87b1e818a953b43a25056ac44189795c7896f406
Icln <gksmfthsus35@naver.com>
2023-03-05 23:23:27 +0900

    feat: Dto 추가, 테스트코드 작성

---
+++ src/test/java/com/example/aws/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,20 @@
+package com.example.aws.web.dto;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void lombok_test(){
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name,amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
