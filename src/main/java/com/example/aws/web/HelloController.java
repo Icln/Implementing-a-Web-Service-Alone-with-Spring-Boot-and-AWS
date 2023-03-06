e9280193466a3d2f8583252f869e729a94bb869b
Icln <gksmfthsus35@naver.com>
2023-03-06 23:22:25 +0900

    feat: 레포지토리 추가, 테스트

--- src/main/java/com/example/aws/web/HelloController.java
+++ src/main/java/com/example/aws/web/HelloController.java
@@ -1,12 +1,15 @@
 package com.example.aws.web;
 
+import com.example.aws.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @GetMapping("/hello")
-    public String hello(){
-        return "hello";
+    public HelloResponseDto hello(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name,amount);
     }
 }
---
+++ src/test/java/com/example/aws/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.example.aws.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        //given
+        String title = "테스트 게시글";
+        String content =  "테스트 본문";
+        Posts newPost = Posts.builder().title(title).content(content).author("jjoa0817@daum.net").build();
+        postsRepository.save(newPost);
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
