8d9b49383be5129697946b6eec2ee4b615ab2166
Icln <gksmfthsus35@naver.com>
2023-03-16 01:33:59 +0900

    feat: IndexController 개선

--- src/main/java/com/example/aws/web/IndexController.java
+++ src/main/java/com/example/aws/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.aws.web;
 
+import com.example.aws.config.auth.LoginUser;
 import com.example.aws.config.auth.dto.SessionUser;
 import com.example.aws.service.posts.PostsService;
 import com.example.aws.web.dto.PostsResponseDto;
@@ -9,18 +10,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
+
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser)  httpSession.getAttribute("user");
 
         if (user != null){
             model.addAttribute("userName", user.getName());
