d9bddd643a4dd78ffc69104ff7c93bf0097c2592
Icln <gksmfthsus35@naver.com>
2023-03-20 23:05:13 +0900

    feat: 임시 수정

--- src/main/java/com/example/aws/AwsApplication.java
+++ src/main/java/com/example/aws/AwsApplication.java
@@ -5,11 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class AwsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AwsApplication.class, args);
 	}
-
 }
