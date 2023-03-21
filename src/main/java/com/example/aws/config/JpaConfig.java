3d4da5d5cb6953ed9d1da873b0ae4487c7cf816d
Icln <gksmfthsus35@naver.com>
2023-03-21 21:55:42 +0900

    feat: 네이버 로그인

--- src/main/java/com/example/aws/config/JpaConfig.java
+++ src/main/java/com/example/aws/config/JpaConfig.java
@@ -3,6 +3,7 @@ package com.example.aws.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @Configuration
 public class JpaConfig {
 }
--- src/main/java/com/example/aws/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/example/aws/config/auth/dto/OAuthAttributes.java
@@ -25,9 +25,9 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
-//        if("naver".equals(registrationId)) {
-//            return ofNaver("id", attributes);
-//        }
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
 
         return ofGoogle(userNameAttributeName, attributes);
     }
@@ -42,17 +42,17 @@ public class OAuthAttributes {
                 .build();
     }
 
-//    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
-//        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
-//
-//        return OAuthAttributes.builder()
-//                .name((String) response.get("name"))
-//                .email((String) response.get("email"))
-//                .picture((String) response.get("profile_image"))
-//                .attributes(response)
-//                .nameAttributeKey(userNameAttributeName)
-//                .build();
-//    }
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
 
     public User toEntity() {
         return User.builder()
