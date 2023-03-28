c07dc994058fe42d348f17cd8bb48bdb0fc8724f
Icln <gksmfthsus35@naver.com>
2023-03-28 22:49:04 +0900

    feat:충돌테스트

--- src/main/java/com/example/aws/web/HelloController.java
+++ src/main/java/com/example/aws/web/HelloController.java
@@ -15,6 +15,7 @@ public class HelloController {
 
     @GetMapping("/bye")
     public HelloResponseDto bye(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        System.out.println("v1");
         return new HelloResponseDto(name,amount);
     }
 
